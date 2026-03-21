009b70642d1b2e1820c0d49f19432adeb8360a7e
Arpit <arpit.m.official@gmail.com>
2026-03-21 15:36:00 +0530

    Iteration 3: Added missing module docstring and fixed singleton comparison

--- whitebox/moneypoly/moneypoly/board.py
+++ whitebox/moneypoly/moneypoly/board.py
@@ -1,3 +1,6 @@
+"""
+Module defining the game Board and its tiles.
+"""
 from moneypoly.property import Property, PropertyGroup
 from moneypoly.config import (
     JAIL_POSITION,
@@ -105,7 +108,7 @@ class Board:
         prop = self.get_property_at(position)
         if prop is None:
             return False
-        if prop.is_mortgaged == True:
+        if prop.is_mortgaged is True:
             return False
         return prop.owner is None
 
